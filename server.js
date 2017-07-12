5ed76b073317a0890a48a08e64e82fb7c3353b4a
karen-edith <karen.edith.garcia1@gmail.com>
2017-07-12 15:37:07 -0400

    made some changes

--- server.js
+++ server.js
@@ -1,21 +1,15 @@
 var http = require('http'); //includes the http module
-var prompt = require('prompt');
 console.log('starting');
-var host = '10.0.0.4';
 var port = 17500;
-
 var x = 10;
 var y = x*100;
 
-var server = http.createServer(function(request, response){
-	console.log('Received request: ' + request.url);
-	response.writeHead(200, {'content-type':'text/plain'}); //writing header
-	response.write('hello world!'+ y);
-	response.end(); // we could do response.end('Hello World!')
-}); 
+function onRequest(request, response){
+	console.log("A User has made a request" + request.url);
+	response.writeHead(200,{"Context-Type":"text/plain"});
+	response.write("Hello world " + y);
+	response.end();
+}
 
-//we need to listen
-server.listen(port, host, function(){
- 	console.log('listening' + host + ':' + port);
-
-});
+http.createServer(onRequest).listen(8888);
+console.log("server is now running")
